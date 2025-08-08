99629067e0a1b0188c621a19a265460ca93c09fe
Mahadev133 <mr.mahadevwadgire@gmail.com>
2025-08-08 19:01:26 +0530

    Add files via upload

--- dual_port_memory.v
+++ dual_port_memory.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 16.06.2025 12:20:59
+// Create Date: 02.07.2025 22:35:07
 // Design Name: 
 // Module Name: dual_port_memory
 // Project Name: 
@@ -20,7 +20,33 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module dual_port_memory(
+module dual_port_memory #(
+    parameter ADDR_WIDTH = 4,
+    parameter DATA_WIDTH = 8
+)(
+    input wire                    wr_clk,
+    input wire                    rd_clk,
+    input wire                    wr_en,
+    input wire                    rd_en,
+    input wire [ADDR_WIDTH-1:0]   wr_addr,
+    input wire [ADDR_WIDTH-1:0]   rd_addr,
+    input wire [DATA_WIDTH-1:0]   din,
+    output reg [DATA_WIDTH-1:0]   dout
+);
 
-    );
+    // Memory declaration
+    reg [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
+
+    // Write operation
+    always @(posedge wr_clk) begin
+        if (wr_en)
+            mem[wr_addr] <= din;
+    end
+
+    // Read operation
+    always @(posedge rd_clk) begin
+        if (rd_en)
+            dout <= mem[rd_addr];
+    end
+
 endmodule
